1cbdb276b2b019067831a5f802cc016e229ad2dc
AndyVish <andrewvish17@gmail.com>
2018-11-18 21:02:47 +0700

    вывод меню ( осн. операции), выбор  операций, инициализация переменной shop - контроль операций.

--- lab_2/Program.cs
+++ lab_2/Program.cs
@@ -19,19 +19,54 @@ namespace lab_2
         void Update();
     }
 
-
-   
-    
-
     class Program
     {
-
+        static public void ShowMenu()
+        {
+            Console.WriteLine("Add[1], Remove[2], Update[3], Show[4], Exit[5]");
+        }
 
         static void Main(string[] args)
         {
-            
-            
+            string exit = "1";
+            Shop shop = new Shop();
 
+            while (exit != "5")
+            {
+                //Console.WriteLine("\t\t====== {0} ======", shop.Name);
+                ShowMenu();
+
+                exit = Console.ReadLine();
+                if (exit != "5")
+                {
+                    if (shop.CountSection > 0)
+                    {
+                        switch (exit)
+                        {
+                            case ("1"):     break;
+                            case ("2"): Console.WriteLine("Remove"); break;
+                            case ("3"): Console.WriteLine("Update"); break;
+                            case ("4"):
+                               
+
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You havent any section. Create new (y/n)?");
+                        if (Console.ReadKey(true).KeyChar == 'y')
+                        {
+                            Section section = new Section();
+                            shop.Add(section);
+                        }
+                    }
+
+
+
+                }
+            }
         }
     }
 }
--- lab_2/Shop.cs
+++ lab_2/Shop.cs
@@ -9,32 +9,20 @@ namespace lab_2
     class Shop
     {
         int countSection;
-        int countCategory;
         List<Section> sectionList;
         
         public Shop()
         {
             countSection = 0;
-            countCategory = 0;
             sectionList = new List<Section>();
         }
 
         public int CountShop { get { return countSection; } }
-        public int CountCategory { get { return countCategory; } }
+        public int CountSection{ get { return countSection; } }
 
         public void Add(Object obj)
         {
-            if (obj is Section)
-            {
-                Section tmp = (Section)obj;
-                sectionList.Add(tmp);
-                countSection++;
-            }
-            else if (obj is Category)
-            {
-                Category tmp = (Category)obj;
-                countCategory++;
-            }
+           
         }
 
         public void Remove(Object obj) {
