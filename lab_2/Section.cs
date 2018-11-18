3af4aef182ec3777010335b0fa6c0477dfad4623
AndyVish <andrewvish17@gmail.com>
2018-11-18 19:24:57 +0700

    Добавил поля и методы в класс Section, удалил categoryList из Shop

--- lab_2/Section.cs
+++ lab_2/Section.cs
@@ -8,7 +8,69 @@ namespace lab_2
 {
     class Section : CommonShopObject
     {
+        int countCategory;
+        List<Category> categoryList;
+        public Section()
+        {
+            
+            categoryList = new List<Category>();
+            countCategory++;
+        }
+        
+        public int CountCategory { get { return countCategory; } }
+        private void CategoryAdd()
+        {
+            Category category = new Category();
+            category.Update();
+            categoryList.Add(category);
+        }
 
+        private void Remove(int index)
+        {
+            categoryList.Remove(categoryList[index]);
+        }
+        // public override void Show() { Console.WriteLine(name); }
+        //public override void Update()
+        //{
+        //    Console.WriteLine("Enter new name");
+        //    string tmp_name = Console.ReadLine();
+        //    if (tmp_name.Trim() != "")
+        //    {
+        //        name = tmp_name;
+        //    }
+        //}
+
+
+
+
+        //public string Name { get { return name; } }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return name;
+        }
+        //    string sectionName;
+        //    public Section() { }
+        //    //List<>
+        //    public Section(string name, int id, string sectName) : base(name, id)
+        //    {
+        //        sectionName = sectName;
+        //    }
+
+        //    string GetSection()
+        //    {
+        //        return sectionName;
+        //    }
+
+        //    public void Show()
+        //    {
+        //        Console.WriteLine("id = {0}, name = {1}, count = {2}, secName = {3}", Id, Name, Count, GetSection());
+        //    }
+        //    public void Update()
+        //    {
+        //        Console.WriteLine("Update");
+        //    }
     }
 }
 
--- lab_2/Shop.cs
+++ lab_2/Shop.cs
@@ -6,9 +6,52 @@ using System.Threading.Tasks;
 
 namespace lab_2
 {
-    class Shop : IShopBaseMethodOne
+    class Shop
     {
-
-    }
         
+        int countSection;
+        int countCategory;
+        List<Section> sectionList;
+        
+        public Shop()
+        {
+            countSection = 0;
+            countCategory = 0;
+            sectionList = new List<Section>();
+        }
+
+        public int CountShop { get { return countSection; } }
+        public int CountCategory { get { return countCategory; } }
+
+        public void Add(Object obj)
+        {
+            if (obj is Section)
+            {
+                Section tmp = (Section)obj;
+                sectionList.Add(tmp);
+                countSection++;
+            }
+            else if (obj is Category)
+            {
+                Category tmp = (Category)obj;
+                countCategory++;
+            }
+        }
+
+        public void Remove(Object obj) {
+
+        }
+
+        public void ShowSectionList()
+        {
+            
+        }
+
+        public void ShowCategoryList()
+        {
+
+        }
+               
+    }
+
 } 
