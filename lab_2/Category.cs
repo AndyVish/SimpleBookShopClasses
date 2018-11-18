11b8fee325f87069189aaff86b8c4258bd0474e6
AndyVish <andrewvish17@gmail.com>
2018-11-18 19:55:12 +0700

    добавил свойство для вывода количества книг в категории;  метод для добавления и удаления книг в список.

--- lab_2/Category.cs
+++ lab_2/Category.cs
@@ -19,6 +19,19 @@ namespace lab_2
             countBook++;
         }
 
-        
+        public int CountBook { get { return countBook; } }
+
+        private void BookAdd()
+        {
+            Book book = new Book();
+            book.Update();
+            bookList.Add(book);
+        }
+
+        private void BookRemove(int index)
+        {
+            bookList.Remove(bookList[index]);
+        }
+
     }
 }
