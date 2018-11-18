3ea08fc10b7a61c4a6d7d71434a2d56c4befe3b5
AndyVish <andrewvish17@gmail.com>
2018-11-18 20:10:04 +0700

    добавил поля, релизацию методов интерфейса в класс Item.

--- lab_2/Item.cs
+++ lab_2/Item.cs
@@ -8,6 +8,23 @@ namespace lab_2
 {
     abstract class Item : IShopBaseMethodSecond
     {
-       
+        protected int id;
+        protected string itemName;
+        protected double price;
+
+        
+        virtual public void Show()
+        {
+            Console.WriteLine("{0}, {1}", itemName, price);
+        }
+        public void Update()
+        {
+            Console.WriteLine("Enter name");
+            string tmp_name = Console.ReadLine();
+            if (tmp_name.Trim() != "")
+            {
+                itemName = tmp_name;
+            }
+        }
     }
 }
